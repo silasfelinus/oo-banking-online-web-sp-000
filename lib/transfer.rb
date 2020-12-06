5ffca314fe87d7c3cb7a3a408fdca73b30cb9b31
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-06 02:14:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
   attr_accessor :sender, :receiver, :status
 
-  def initialize(sender, receiver)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = pending
